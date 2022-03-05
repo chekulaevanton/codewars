601fe09d8c8ae190d448de9afa05c8414f0c7721
Anton Chekulaev <chekulaevanton1@gmail.com>
2022-03-05 21:10:23 +0400

    Solved again sumStringsAsNumbers

--- javascript/4kyu/sumStringsAsNumbers.js
+++ javascript/4kyu/sumStringsAsNumbers.js
@@ -1,32 +1,6 @@
-function sumStrings(a, b) {
-  const normalizeNum = function(arr) {
-    let firstNotZeroIndex;
-    for (let i = arr.length - 1; i >= 0; i--) {
-      if (arr[i] !== '0') {
-        firstNotZeroIndex = i;
-        break;
-      }
-    }
-    return arr.slice(0, firstNotZeroIndex + 1);
-  };
-
-  let firstNum = normalizeNum(a.split('').reverse());
-  let secondNum = normalizeNum(b.split('').reverse());
-  let maxLength = Math.max(firstNum.length, secondNum.length);
-
-  let carry = 0;
-  let result = [];
-
-  for (let i = 0; i <= maxLength; i++) {
-    let firstDigit = +firstNum[i] || 0;
-    let secondDigit = +secondNum[i] || 0;
-
-    let resultDigit = (firstDigit + secondDigit + carry) % 10;
-    carry = Math.trunc((firstDigit + secondDigit + carry) / 10);
-
-    if (i === maxLength && resultDigit === 0) continue;
-    result.push(resultDigit.toString());
-  }
-
-  return result.reverse().join('');
+// OK -_-
+function sumStrings(a,b) {
+  return (BigInt(a) + BigInt(b)).toString();
 }
+
+console.log(sumStrings('123','456')); // '579'
